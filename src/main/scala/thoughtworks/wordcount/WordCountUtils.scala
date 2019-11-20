b8492c7fe693fbc8321632ce77f782b4cee1988c
Emily <eleones@thoughtworks.com>
2019-11-20 14:12:25 +0530

    Brian / Emily | Use Spark to count words in a file.

--- src/main/scala/thoughtworks/wordcount/WordCountUtils.scala
+++ src/main/scala/thoughtworks/wordcount/WordCountUtils.scala
@@ -1,16 +1,22 @@
 package thoughtworks.wordcount
 
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
 
 object WordCountUtils {
   implicit class StringDataset(val dataSet: Dataset[String]) {
-    def splitWords(spark: SparkSession) = {
+    def splitWords(spark: SparkSession): Dataset[String] = {
+      import spark.implicits._
       dataSet
+        .flatMap(_.split("[^\\w']"))
+        .filter(!_.equals(""))
     }
 
     def countByWord(spark: SparkSession) = {
       import spark.implicits._
       dataSet.as[String]
+        .groupByKey(_.toLowerCase)
+        .count
+        .orderBy($"value")
     }
   }
 }
--- src/test/scala/thoughtworks/wordcount/WordCountUtilsTest.scala
+++ src/test/scala/thoughtworks/wordcount/WordCountUtilsTest.scala
@@ -1,33 +1,152 @@
 package thoughtworks.wordcount
 
 import WordCountUtils._
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{Dataset, Encoders}
 import thoughtworks.DefaultFeatureSpecWithSpark
-
 
 class WordCountUtilsTest extends DefaultFeatureSpecWithSpark {
   feature("Split Words") {
-    ignore("test splitting a dataset of words by spaces") {}
+    scenario("test splitting a dataset of words by spaces") {
+      import spark.implicits._
+      Given("A dataset with simple values and whitespaces")
+      val inputList = List(
+        "one     two three"
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
 
-    ignore("test splitting a dataset of words by period") {}
+      When("I split on the whitespaces")
+      val result = dataset.splitWords(spark).collectAsList()
 
-    ignore("test splitting a dataset of words by comma") {}
+      Then("I have a list of words")
+      result should contain theSameElementsAs List("one", "two", "three")
+    }
 
-    ignore("test splitting a dataset of words by hypen") {}
+    scenario("test splitting a dataset of words by period") {
+      import spark.implicits._
+      Given("A dataset with simple values, whitespace, and periods")
+      val inputList = List(
+        "one.two. three"
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
 
-    ignore("test splitting a dataset of words by semi-colon") {}
+      When("I split the words")
+      val result = dataset.splitWords(spark).collectAsList()
+
+      Then("I have a list of words")
+      result should contain theSameElementsAs List("one", "two", "three")
+    }
+
+    scenario("test splitting a dataset of words by comma") {
+      import spark.implicits._
+      Given("A dataset with simple values, whitespace, and periods")
+      val inputList = List(
+        "one,two,. three"
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
+
+      When("I split on the words")
+      val result = dataset.splitWords(spark).collectAsList()
+
+      Then("I have a list of words")
+      result should contain theSameElementsAs List("one", "two", "three")
+    }
+
+    scenario("test splitting a dataset of words by hypen") {
+      import spark.implicits._
+      Given("A dataset with simple values, whitespace, hyphens, and periods")
+      val inputList = List(
+        "one-two,- three. "
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
+
+      When("I split on the words")
+      val result = dataset.splitWords(spark).collectAsList()
+
+      Then("I have a list of words")
+      result should contain theSameElementsAs List("one", "two", "three")
+    }
+
+    scenario("test splitting a dataset of words by semi-colon") {
+      import spark.implicits._
+      Given("A dataset with simple values, whitespace, hyphens, semi-colons, and periods")
+      val inputList = List(
+        "one;two;. three; "
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
+
+      When("I split on the words")
+      val result = dataset.splitWords(spark).collectAsList()
+
+      Then("I have a list of words")
+      result should contain theSameElementsAs List("one", "two", "three")
+    }
   }
 
   feature("Count Words") {
-    ignore("basic test case") {}
+    scenario("basic test case") {
+      import spark.implicits._
+      Given("A dataset with a single value")
+      val inputList = List(
+        "one"
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
 
-    ignore("should not aggregate dissimilar words") {}
+      When("I count the words")
+      val result = dataset.countByWord(spark).collectAsList()
 
-    ignore("test case insensitivity") {}
+      Then("I have a count of each word")
+      result should contain theSameElementsAs List(("one",1))
+    }
+
+    scenario("should not aggregate dissimilar words") {
+      import spark.implicits._
+      Given("A dataset with dissimilar values")
+      val inputList = List(
+        "one", "two", "two"
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
+
+      When("I count the words")
+      val result = dataset.countByWord(spark).collectAsList()
+
+      Then("I have a count of each word")
+      result should contain theSameElementsAs List(("one",1), ("two",2))
+    }
+
+    scenario("test case insensitivity") {
+      import spark.implicits._
+      Given("A dataset with similar values of different cases")
+      val inputList = List(
+        "oNe", "one", "TwO", "two"
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
+
+      When("I count the words")
+      val result = dataset.countByWord(spark).collectAsList()
+
+      Then("I have a count of each word")
+      result should contain theSameElementsAs List(("one",2), ("two",2))
+    }
   }
 
   feature("Sort Words") {
-    ignore("test ordering words") {}
+    scenario("test ordering words") {
+      import spark.implicits._
+      Given("A dataset with values not in alphabetical order")
+      val inputList = List(
+        "zebra","banana", "app", "apple", "banana"
+      )
+      val dataset: Dataset[String] = spark.createDataset(inputList)
+      dataset.show()
+
+      When("I count the words")
+      val result = dataset.countByWord(spark).collectAsList()
+      dataset.countByWord(spark).show()
+
+      Then("I have a count of each word")
+      println(result)
+      result should contain theSameElementsInOrderAs List(("app", 1), ("apple",1), ("banana",2), ("zebra", 1))
+    }
   }
 
 }
